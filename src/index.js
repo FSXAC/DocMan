64db387fa45613e9c77864889eb4271cd751c78b
Muchen He <i@muchen.ca>
2018-05-31 18:19:59 -0600

    Initial commit can load JSON

---
+++ src/index.js
@@ -0,0 +1,81 @@
+const electron = require('electron');
+const { ipcRenderer } = electron;
+
+const statusText = document.querySelector('#status-text');
+
+const categoryList = document.querySelector('#category-list');
+const courseList = document.querySelector('#course-list');
+const documentList = document.querySelector('#document-list');
+
+let g_docData;
+
+// TODO: to be removed
+ipcRenderer.on('document:add', (e, item)=>{
+	const li = document.createElement('li');
+	const itemText = document.createTextNode(item);
+	li.appendChild(itemText);
+	documentList.appendChild(li);
+});
+
+ipcRenderer.on('manifest:load', (e, data)=>{
+	statusText.innerHTML = 'loaded manifest';
+	g_docData = data.docs;
+
+	clearList(categoryList);
+	clearList(courseList);
+	clearList(documentList);
+	populateCategories();
+});
+
+function clearList(list) {
+	while (list.firstChild) {
+		list.removeChild(list.firstChild);
+	}
+}
+
+function populateCategories() {
+	g_docData.forEach(element => {
+		const newCategory = document.createElement('li');
+		newCategory.appendChild(document.createTextNode(element.category));
+
+		newCategory.addEventListener('click', ()=> {
+			clearList(courseList);
+			clearList(documentList);
+			populateCourses(element.courses);
+		});
+
+		categoryList.appendChild(newCategory);
+	});
+}
+
+function populateCourses(courses) {
+	courses.forEach(element => {
+		const newCourse = document.createElement('li');
+		newCourse.appendChild(document.createTextNode(element.course + ': ' + element.description));
+
+		newCourse.addEventListener('click', ()=> {
+			clearList(documentList);
+			populateEntries(element.entries);
+		});
+
+		courseList.appendChild(newCourse);
+	});
+}
+
+function populateEntries(entries) {
+	entries.forEach(element => {
+		const newEntry = document.createElement('li');
+
+		if (element.title !== undefined && element.title !== null && element.title !== '') {
+			newEntry.appendChild(document.createTextNode(element.title));
+			documentList.appendChild(newEntry);
+		}
+
+	});
+
+	if (documentList.firstChild === null || documentList.firstChild === undefined) {
+		const textNA = document.createElement('i');
+		textNA.appendChild(document.createTextNode('No notes'));
+		documentList.appendChild(textNA);
+	}
+}
---
+++ src/main.js
@@ -0,0 +1,134 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const { app, BrowserWindow, Menu, ipcMain } = electron;
+const { dialog } = electron;
+
+let mainWindow;
+let addDocumentWindow;
+
+// Listen for app ready
+app.on('ready', createWindow);
+
+function createWindow() {
+    // Create new window
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+    mainWindow.on('closed', ()=>{
+        mainWindow = null;
+    });
+
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+}
+
+function createAddDocumentWindow() {
+    addDocumentWindow = new BrowserWindow({ width: 400, height: 400 });
+    addDocumentWindow.setMenu(null);
+    addDocumentWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addDocument.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+    addDocumentWindow.on('closed', ()=>{
+        addDocumentWindow = null;
+    });
+}
+
+ipcMain.on('document:add', (e, item)=>{
+    mainWindow.webContents.send('document:add', item);
+    addDocumentWindow.close();
+});
+
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu:[
+            {
+                label: 'Open Manifest',
+                accelerator: 'Ctrl+O',
+                click() { openOpenManifestDialog(); }
+            },
+            {
+                label: 'Add document',
+                accelerator: 'Ctrl+N',
+                click() { createAddDocumentWindow(); }
+            },
+            {
+                label: 'Quit',
+                accelerator: 'Ctrl+Q',
+                click() { app.quit(); }
+            }
+        ]
+    },
+    {
+        label: 'Edit',
+        submenu: [
+            { role: 'undo' },
+            { role: 'redo' },
+            { type: 'separator' },
+            { role: 'cut' },
+            { role: 'copy' },
+            { role: 'paste' },
+            { role: 'pasteandmatchstyle' },
+            { role: 'delete' },
+            { role: 'selectall' }
+        ]
+    },
+    {
+        label: 'View',
+        submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { role: 'toggledevtools' },
+            { type: 'separator' },
+            { role: 'resetzoom' },
+            { role: 'zoomin' },
+            { role: 'zoomout' },
+            { type: 'separator' },
+            { role: 'togglefullscreen' }
+        ]
+    },
+    {
+        role: 'help',
+        submenu: [
+            {
+                label: 'Learn More',
+                click() { require('electron').shell.openExternal('https://electronjs.org') }
+            }
+        ]
+    }
+];
+
+if (process.env.NODE_ENV !== 'production') {
+    mainMenuTemplate.push({
+        label: 'Developer'
+    });
+}
+
+app.on('window-all-closed', ()=>{
+    app.quit()
+});
+
+app.on('active', ()=>{
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+
+function openOpenManifestDialog() {
+    let manifestPath = dialog.showOpenDialog({
+        properties: ['openFile'],
+        filters: [
+            {name: 'Document manifests', extensions: ['json', 'docman']}
+        ]
+    })[0];
+
+    const data = require(manifestPath);
+    mainWindow.webContents.send('manifest:load', data);
+}
